6ddc278381533b5d8b0c7983c04c7432e971825d
unknown <renstrom.peter@gmail.com>
2010-11-15 18:29:41 +0100

    Some changes and tweaks to the config file

--- config.lua
+++ config.lua
@@ -1,69 +1,79 @@
 local addonName, addon = ...
 
-local font = "Fonts\\ARIALN.ttf"
+-- Use these local values for parenting internally among the data texts
+local Durability = addon.stat.durability
+local FPS = addon.stat.fps
+local Friend = addon.stat.friend
+local Gold = addon.stat.gold
+local Guild = addon.stat.guild
+local Memory = addon.stat.memory
+
+-- Set font and size for all data texts
+local font = 'Fonts\\ARIALN.ttf'
 local font_size = 12
 
 -- Displays durability on equipped items
 addon.settings.durability = {
-	["enable"] = false,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -5, 20},
-	["tooltip_position"] = {"ANCHOR_TOP", 0, 6},
-	["class_color"] = true,
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'BOTTOMRIGHT', Gold, 'TOPRIGHT', 0, 5},
+	tooltip_position 	= {'ANCHOR_TOP', 0, 6},
+	class_color 		= true,
 }
 
 -- Shows online friends
 addon.settings.friend = {
-	["enable"] = true,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"TOPRIGHT", Minimap, "BOTTOMRIGHT", -5, -5},
-	["tooltip_position"] = {"ANCHOR_BOTTOM", 0, -6},
-	["class_color"] = true,
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'TOPRIGHT', Minimap, 'BOTTOMRIGHT', -5, -5},
+	tooltip_position 	= {'ANCHOR_BOTTOM', 0, -6},
+	class_color 		= true,
 }
 
 -- Shows online guild members, right-click to send a whisper or invite
 addon.settings.guild = {
-	["enable"] = true,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"TOPLEFT", Minimap, "BOTTOMLEFT", 5, -5},
-	["tooltip_position"] = {"ANCHOR_BOTTOM", 0, -6},
-	["class_color"] = true,
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'TOPLEFT', Minimap, 'BOTTOMLEFT', 5, -5},
+	tooltip_position 	= {'ANCHOR_BOTTOM', 0, -6},
+	class_color 		= true,
 }
 
 -- Displays amount of gold your characters, and shows gold gained/lost in current session
 addon.settings.gold = {
-	["enable"] = true,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -5, 5},
-	["tooltip_position"] = {"ANCHOR_TOP", 0, 6},
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'BOTTOMRIGHT', UIParent, 'BOTTOMRIGHT', -5, 20},
+	tooltip_position 	= {'ANCHOR_TOP', 0, 6},
+	class_color 		= true,
 }
 
 -- Displays memory and bandwidth usage of addons
 addon.settings.memory = {
-	["enable"] = false,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -5, 20},
-	["tooltip_position"] = {"ANCHOR_TOP", 0, 6},
-	["class_color"] = true,
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'BOTTOMRIGHT', Durability, 'TOPRIGHT', 0, 5},
+	tooltip_position 	= {'ANCHOR_TOP', 0, 6},
+	class_color 		= true,
 }
 
 -- Displays framerate and memory usage
 addon.settings.fps = {
-	["enable"] = false,
+	enable 				= true,
 	
-	["font"] = font,
-	["font_size"] = 12,
-	["position"] = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -5, 20},
-	["tooltip_position"] = {"ANCHOR_TOP", 0, 6},
-	["class_color"] = true,
+	font 				= font,
+	font_size 			= 12,
+	position 			= {'BOTTOMRIGHT', Memory, 'TOPRIGHT', 0, 5},
+	tooltip_position 	= {'ANCHOR_TOP', 0, 6},
+	class_color 		= true,
 }
