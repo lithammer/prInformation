163078a1623d37042938c49be1abbc8794b03a72
ballagarba <renstrom.peter@gmail.com>
2011-02-15 12:33:31 +0100

    Some updates for 4.0.6 and minor tweaks

--- modules/memory.lua
+++ modules/memory.lua
@@ -3,12 +3,12 @@ local addonName, addon = ...
 if not addon.settings.memory.enable then return end
 
 local Stat = addon.stat.memory
+Stat:RegisterEvent('PLAYER_ENTERING_WORLD')
 Stat:EnableMouse(true)
-Stat:SetFrameStrata("BACKGROUND")
+Stat:SetFrameStrata('BACKGROUND')
 Stat:SetFrameLevel(3)
-local colorme = string.format("%02x%02x%02x", 1*255, 1*255, 1*255)
 
-local Text  = Stat:CreateFontString(nil, "OVERLAY")
+local Text  = Stat:CreateFontString(nil, 'OVERLAY')
 Text:SetFont(addon.settings.memory.font, addon.settings.memory.font_size)
 Text:SetPoint(unpack(addon.settings.memory.position))
 Text:SetShadowColor(0, 0, 0)
@@ -18,91 +18,97 @@ if addon.settings.memory.class_color then
 	Text:SetTextColor(RAID_CLASS_COLORS[class].r, RAID_CLASS_COLORS[class].g, RAID_CLASS_COLORS[class].b )
 end
 
-local function formatMem(memory, color)
-	if color then
-		statColor = { "|cff"..colorme, "|r" }
-	else
-		statColor = { "", "" }
-	end
-	
+local bandwidthString = '%.2f Mbps'
+local percentageString = '%.2f%%'
+
+local kiloByteString = '%d |cffffffffkb|r'
+local megaByteString = '%.2f |cffffffffmb|r'
+
+local function formatMem(memory)
 	local mult = 10^1
 	if memory > 999 then
-		local mem = floor((memory/1024) * mult + 0.5) / mult
-		if mem % 1 == 0 then
-			return mem..string.format(".0 %smb%s", unpack(statColor))
-		else
-			return mem..string.format(" %smb%s", unpack(statColor))
-		end
+		local mem = ((memory/1024) * mult) / mult
+		return string.format(megaByteString, mem)
 	else
-		local mem = floor(memory * mult + 0.5) / mult
-			if mem % 1 == 0 then
-				return mem..string.format(".0 %skb%s", unpack(statColor))
-			else
-				return mem..string.format(" %skb%s", unpack(statColor))
-			end
+		local mem = (memory * mult) / mult
+		return string.format(kiloByteString, mem)
 	end
 end
 
-local Total, Mem, MEMORY_TEXT, LATENCY_TEXT, Memory
-local function RefreshMem(self)
-	Memory = {}
-	UpdateAddOnMemoryUsage()
-	Total = 0
-	for i = 1, GetNumAddOns() do
-		Mem = GetAddOnMemoryUsage(i)
-		Memory[i] = { select(2, GetAddOnInfo(i)), Mem, IsAddOnLoaded(i) }
-		Total = Total + Mem
+local memoryTable = {}
+
+local function RebuildAddonList(self)
+	local addOnCount = GetNumAddOns()
+	if (addOnCount == #memoryTable) or self.tooltip == true then return end
+
+	-- Number of loaded addons changed, create new memoryTable for all addons
+	memoryTable = {}
+	for i = 1, addOnCount do
+		memoryTable[i] = { i, select(2, GetAddOnInfo(i)), 0, IsAddOnLoaded(i) }
 	end
-	
-	MEMORY_TEXT = formatMem(Total, true)
-	table.sort(Memory, function(a, b)
-		if a and b then
-			return a[2] > b[2]
-		end
-	end)
-	
 	self:SetAllPoints(Text)
 end
 
-local int, int2 = 10, 1
+local function UpdateMemory()
+	-- Update the memory usages of the addons
+	UpdateAddOnMemoryUsage()
+	-- Load memory usage in table
+	local addOnMem = 0
+	local totalMemory = 0
+	for i = 1, #memoryTable do
+		addOnMem = GetAddOnMemoryUsage(memoryTable[i][1])
+		memoryTable[i][3] = addOnMem
+		totalMemory = totalMemory + addOnMem
+	end
+	-- Sort the table to put the largest addon on top
+	table.sort(memoryTable, function(a, b)
+		if a and b then
+			return a[3] > b[3]
+		end
+	end)
+	
+	return totalMemory
+end
+
+local int = 10
+
 local function Update(self, t)
 	int = int - t
-	int2 = int2 - t
+	
 	if int < 0 then
-		RefreshMem(self)
+		RebuildAddonList(self)
+		local total = UpdateMemory()
+		Text:SetText(formatMem(total))
 		int = 10
-	end
-	if int2 < 0 then
-		Text:SetText(MEMORY_TEXT)
-		int2 = 1
 	end
 end
 
-Stat:SetScript("OnMouseDown", function() collectgarbage("collect") Update(Stat, 20) end)
-Stat:SetScript("OnUpdate", Update) 
-Stat:SetScript("OnEnter", function(self)
+Stat:SetScript('OnMouseDown', function () collectgarbage('collect') Update(Stat, 10) end)
+Stat:SetScript('OnEnter', function(self)
 	if not InCombatLockdown() then
+		self.tooltip = true
 		local bandwidth = GetAvailableBandwidth()
-		GameTooltip:SetOwner(self, unpack(addon.settings.memory.tooltip_position));
-		GameTooltip:ClearAllPoints()
-		GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, 0)
+		GameTooltip:SetOwner(self, unpack(addon.settings.memory.tooltip_position))
 		GameTooltip:ClearLines()
 		if bandwidth ~= 0 then
-			GameTooltip:AddDoubleLine("Bandwidth: ", format("%s ".."Mbps", bandwidth), 0.69, 0.31, 0.31,0.84, 0.75, 0.65)
-			GameTooltip:AddDoubleLine("Download: ", format("%s%%", floor(GetDownloadedPercentage()*100+0.5)), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
-			GameTooltip:AddLine(" ")
+			GameTooltip:AddDoubleLine('Bandwidth: ', string.format(bandwidthString, bandwidth), 0.69, 0.31, 0.31,0.84, 0.75, 0.65)
+			GameTooltip:AddDoubleLine('Download: ', string.format(percentageString, GetDownloadedPercentage() * 100), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
+			GameTooltip:AddLine(' ')
 		end
-		GameTooltip:AddDoubleLine("Total Memory Usage:", formatMem(Total), 0.69, 0.31, 0.31,0.84, 0.75, 0.65)
-		GameTooltip:AddLine(" ")
-		for i = 1, #Memory do
-			if Memory[i][3] then 
-				local red = Memory[i][2] / Total * 2
+		local totalMemory = UpdateMemory()
+		GameTooltip:AddDoubleLine('Total Memory Usage:', formatMem(totalMemory), 0.69, 0.31, 0.31,0.84, 0.75, 0.65)
+		GameTooltip:AddLine(' ')
+		for i = 1, #memoryTable do
+			if (memoryTable[i][4]) then
+				local red = memoryTable[i][3] / totalMemory
 				local green = 1 - red
-				GameTooltip:AddDoubleLine(Memory[i][1], formatMem(Memory[i][2], false), 1, 1, 1, red, green + 1, 0)						
-			end
+				GameTooltip:AddDoubleLine(memoryTable[i][2], formatMem(memoryTable[i][3]), 1, 1, 1, red, green + .5, 0)
+			end						
 		end
 		GameTooltip:Show()
 	end
 end)
-Stat:SetScript("OnLeave", function() GameTooltip:Hide() end)
-Update(Stat, 20)
+Stat:SetScript('OnLeave', function(self) self.tooltip = false GameTooltip:Hide() end)
+Stat:SetScript('OnUpdate', Update)
+Stat:SetScript('OnEvent', function(self, event) collectgarbage('collect') end)
+Update(Stat, 10)
--- prInformation.lua
+++ prInformation.lua
@@ -3,9 +3,26 @@ local addonName, addon = ...
 addon.settings = {}
 addon.stat = {}
 
-addon.stat.durability = CreateFrame("Frame")
-addon.stat.fps = CreateFrame("Frame")
-addon.stat.friend = CreateFrame("Frame")
-addon.stat.gold = CreateFrame("Frame")
-addon.stat.guild = CreateFrame("Frame")
-addon.stat.memory = CreateFrame("Frame")
+addon.stat.durability = CreateFrame('Frame')
+addon.stat.fps = CreateFrame('Frame')
+addon.stat.friend = CreateFrame('Frame')
+addon.stat.gold = CreateFrame('Frame')
+addon.stat.guild = CreateFrame('Frame')
+addon.stat.memory = CreateFrame('Frame')
+
+addon.ShortValue = function(v)
+	if v >= 1e6 then
+		return ('%.1fm'):format(v / 1e6):gsub('%.?0+([km])$', '%1')
+	elseif v >= 1e3 or v <= -1e3 then
+		return ('%.1fk'):format(v / 1e3):gsub('%.?0+([km])$', '%1')
+	else
+		return v
+	end
+end
+
+addon.RGBToHex = function(r, g, b)
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format('|cff%02x%02x%02x', r*255, g*255, b*255)
+end
