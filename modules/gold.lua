5f7df6bfc404fe4292787f1f1c42cc1bb7dbb180
ballagarba <renstrom.peter@gmail.com>
2011-03-08 00:05:31 +0100

    Use GetCoinTextureStrint() instead of custom functions, and cleaned up the code a bit

--- modules/gold.lua
+++ modules/gold.lua
@@ -2,14 +2,16 @@ local addonName, addon = ...
 
 if not addon.settings.gold.enable then return end
 
+local settings = addon.settings.gold
+
 local Stat = addon.stat.gold
 Stat:EnableMouse(true)
 Stat:SetFrameStrata('BACKGROUND')
 Stat:SetFrameLevel(3)
 
 local Text = Stat:CreateFontString(nil, 'OVERLAY')
-Text:SetFont(addon.settings.gold.font, addon.settings.gold.font_size)
-Text:SetPoint(unpack(addon.settings.gold.position))
+Text:SetFont(settings.font, settings.font_size)
+Text:SetPoint(unpack(settings.position))
 Text:SetShadowColor(0, 0, 0)
 Text:SetShadowOffset(1, -1)
 
@@ -18,48 +20,37 @@ local Spent	= 0
 local OldMoney = 0
 local myPlayerRealm = GetCVar('realmName')
 
-local function formatMoney(money)
-	local gold = floor(math.abs(money) / 10000)
-	local silver = mod(floor(math.abs(money) / 100), 100)
-	local copper = mod(floor(math.abs(money)), 100)
-	if gold ~= 0 then
-		return format('%s|cffffd700g|r %s|cffc7c7cfs|r %s|cffeda55fc|r', gold, silver, copper)
-	elseif silver ~= 0 then
-		return format('%s|cffc7c7cfs|r %s|cffeda55fc|r', silver, copper)
-	else
-		return format('%s|cffeda55fc|r', copper)
-	end
-end
-
-local function FormatTooltipMoney(money)
-	local gold, silver, copper = abs(money / 10000), abs(mod(money / 100, 100)), abs(mod(money, 100))
-	local cash = ''
-	cash = format('%.2d|cffffd700g|r %.2d|cffc7c7cfs|r %.2d|cffeda55fc|r', gold, silver, copper)		
-	return cash
-end	
-
 local function OnEvent(self, event)
 	if event == 'PLAYER_ENTERING_WORLD' then
 		OldMoney = GetMoney()
 	end
 	
-	local NewMoney	= GetMoney()
-	local Change = NewMoney-OldMoney -- Positive if we gain money
+	local NewMoney = GetMoney()
+	local Change = NewMoney - OldMoney -- Positive if we gain money
 	
-	if OldMoney>NewMoney then		-- Lost Money
+	if (OldMoney > NewMoney) then	-- Lost Money
 		Spent = Spent - Change
 	else							-- Gained Moeny
 		Profit = Profit + Change
 	end
 	
-	Text:SetText(formatMoney(NewMoney))
+	Text:SetText(GetCoinTextureString(NewMoney, settings.font_size))
 	-- Setup Money Tooltip
 	self:SetAllPoints(Text)
 
-	local myPlayerName  = UnitName('player')				
-	if (prInformationData == nil) then prInformationData = {} end
-	if (prInformationData.gold == nil) then prInformationData.gold = {} end
-	if (prInformationData.gold[myPlayerRealm]==nil) then prInformationData.gold[myPlayerRealm]={} end
+	local myPlayerName = UnitName('player')				
+	if (prInformationData == nil) then
+		prInformationData = {}
+	end
+
+	if (prInformationData.gold == nil) then
+		prInformationData.gold = {}
+	end
+
+	if (prInformationData.gold[myPlayerRealm]==nil) then
+		prInformationData.gold[myPlayerRealm]={}
+	end
+
 	prInformationData.gold[myPlayerRealm][myPlayerName] = GetMoney()
 
 	OldMoney = NewMoney
@@ -74,54 +65,66 @@ Stat:RegisterEvent('PLAYER_ENTERING_WORLD')
 Stat:SetScript('OnMouseDown', function() OpenAllBags() end)
 Stat:SetScript('OnEvent', OnEvent)
 Stat:SetScript('OnEnter', function(self)
-	if not InCombatLockdown() then
+	if (not InCombatLockdown()) then
 		self.hovered = true 
-		GameTooltip:SetOwner(self, unpack(addon.settings.gold.tooltip_position))
+		GameTooltip:SetOwner(self, unpack(settings.tooltip_position))
 		GameTooltip:ClearAllPoints()
 		GameTooltip:SetPoint('BOTTOM', self, 'TOP', 0, 0)
 		GameTooltip:ClearLines()
 		GameTooltip:AddLine('Session: ')
-		GameTooltip:AddDoubleLine('Earned:', formatMoney(Profit), 1, 1, 1, 1, 1, 1)
-		GameTooltip:AddDoubleLine('Spent:', formatMoney(Spent), 1, 1, 1, 1, 1, 1)
+		GameTooltip:AddDoubleLine('Earned:', GetCoinTextureString(Profit), 1, 1, 1, 1, 1, 1)
+		GameTooltip:AddDoubleLine('Spent:', GetCoinTextureString(Spent), 1, 1, 1, 1, 1, 1)
+
 		if (Profit < Spent) then
-			GameTooltip:AddDoubleLine('Deficit:', formatMoney(Profit-Spent), 1, 0, 0, 1, 1, 1)
+			GameTooltip:AddDoubleLine('Deficit:', GetCoinTextureString(Profit - Spent), 1, 0, 0, 1, 1, 1)
 		elseif (Profit - Spent) > 0 then
-			GameTooltip:AddDoubleLine('Profit:', formatMoney(Profit-Spent), 0, 1, 0, 1, 1, 1)
-		end				
+			GameTooltip:AddDoubleLine('Profit:', GetCoinTextureString(Profit - Spent), 0, 1, 0, 1, 1, 1)
+		end
 		GameTooltip:AddLine(' ')					
 	
 		local totalGold = 0				
 		GameTooltip:AddLine('Character: ')			
 		local thisRealmList = prInformationData.gold[myPlayerRealm]
+
 		for k, v in pairs(thisRealmList) do
-			GameTooltip:AddDoubleLine(k, FormatTooltipMoney(v), 1, 1, 1, 1, 1, 1)
+			GameTooltip:AddDoubleLine(k, GetCoinTextureString(v), 1, 1, 1, 1, 1, 1)
 			totalGold = totalGold + v
-		end 
+		end
+
 		GameTooltip:AddLine(' ')
 		GameTooltip:AddLine('Server: ')
-		GameTooltip:AddDoubleLine('Total: ', FormatTooltipMoney(totalGold), 1, 1, 1, 1, 1, 1)
+		GameTooltip:AddDoubleLine('Total: ', GetCoinTextureString(totalGold), 1, 1, 1, 1, 1, 1)
 
 		for i = 1, GetNumWatchedTokens() do
 			local name, count, extraCurrencyType, icon, itemID = GetBackpackCurrencyInfo(i)
-			if name and i == 1 then
+
+			if (name and i == 1) then
 				GameTooltip:AddLine(' ')
 				GameTooltip:AddLine(CURRENCY)
 			end
-			local r, g, b = 1,1,1
-			if itemID then r, g, b = GetItemQualityColor(select(3, GetItemInfo(itemID))) end
-			if name and count then GameTooltip:AddDoubleLine(name, count, r, g, b, 1, 1, 1) end
+
+			local r, g, b = 1, 1, 1
+			if (itemID) then
+				r, g, b = GetItemQualityColor(select(3, GetItemInfo(itemID)))
+			end
+
+			if (name and count) then
+				GameTooltip:AddDoubleLine(name, count, r, g, b, 1, 1, 1)
+			end
 		end
+
 		GameTooltip:Show()
 	end
 end)
-Stat:SetScript('OnLeave', function() GameTooltip:Hide() end)	
+Stat:SetScript('OnLeave', function() GameTooltip:Hide() end)
+
 -- reset gold data
 local function RESETGOLD()
 	local myPlayerRealm = GetCVar('realmName')
 	local myPlayerName  = UnitName('player')
 	
 	prInformationData.gold = {}
-	prInformationData.gold[myPlayerRealm]={}
+	prInformationData.gold[myPlayerRealm] = {}
 	prInformationData.gold[myPlayerRealm][myPlayerName] = GetMoney()
 end
 SLASH_RESETGOLD1 = '/resetgold'
